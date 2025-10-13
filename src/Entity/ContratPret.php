9c624883cd48789409314e5cd922ce8f241e875e
sio2 <villainalexandre89@gmail.com>
2025-10-13 14:27:43 +0200

    création de l'entité ContratPret

---
+++ src/Entity/ContratPret.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContratPretRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContratPretRepository::class)]
+class ContratPret
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateDebut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateFin = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $attestationAssurance = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $etatDetailleDebut = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $etatdetailleRetour = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTime $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTime
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTime $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getAttestationAssurance(): ?string
+    {
+        return $this->attestationAssurance;
+    }
+
+    public function setAttestationAssurance(string $attestationAssurance): static
+    {
+        $this->attestationAssurance = $attestationAssurance;
+
+        return $this;
+    }
+
+    public function getEtatDetailleDebut(): ?string
+    {
+        return $this->etatDetailleDebut;
+    }
+
+    public function setEtatDetailleDebut(string $etatDetailleDebut): static
+    {
+        $this->etatDetailleDebut = $etatDetailleDebut;
+
+        return $this;
+    }
+
+    public function getEtatdetailleRetour(): ?string
+    {
+        return $this->etatdetailleRetour;
+    }
+
+    public function setEtatdetailleRetour(string $etatdetailleRetour): static
+    {
+        $this->etatdetailleRetour = $etatdetailleRetour;
+
+        return $this;
+    }
+}
