0a759de5aa1fdf0aaabf2cc1d88f0bbf49bb24db
sio2 <villainalexandre89@gmail.com>
2025-10-13 14:07:47 +0200

    création de l'entité intervention

---
+++ src/Entity/Intervention.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InterventionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InterventionRepository::class)]
+class Intervention
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateDebut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateFin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $descriptif = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    private ?float $quotite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTime $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTime
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(\DateTime $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function getDescriptif(): ?string
+    {
+        return $this->descriptif;
+    }
+
+    public function setDescriptif(string $descriptif): static
+    {
+        $this->descriptif = $descriptif;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuotite(): ?float
+    {
+        return $this->quotite;
+    }
+
+    public function setQuotite(float $quotite): static
+    {
+        $this->quotite = $quotite;
+
+        return $this;
+    }
+}
