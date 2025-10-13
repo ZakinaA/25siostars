160706e1107c02aabbb278a3d3e2d931ebe5b245
sio2 <villainalexandre89@gmail.com>
2025-10-13 14:16:55 +0200

    création de l'entité Professionnel

---
+++ src/Entity/Professionnel.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProfessionnelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProfessionnelRepository::class)]
+class Professionnel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $numRue = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $rue = null;
+
+    #[ORM\Column]
+    private ?float $copos = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $ville = null;
+
+    #[ORM\Column]
+    private ?float $tel = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $mail = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNumRue(): ?float
+    {
+        return $this->numRue;
+    }
+
+    public function setNumRue(float $numRue): static
+    {
+        $this->numRue = $numRue;
+
+        return $this;
+    }
+
+    public function getRue(): ?string
+    {
+        return $this->rue;
+    }
+
+    public function setRue(string $rue): static
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    public function getCopos(): ?float
+    {
+        return $this->copos;
+    }
+
+    public function setCopos(float $copos): static
+    {
+        $this->copos = $copos;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getTel(): ?float
+    {
+        return $this->tel;
+    }
+
+    public function setTel(float $tel): static
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): static
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+}
