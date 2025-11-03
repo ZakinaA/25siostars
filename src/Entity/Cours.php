65c550e97904e7afecefd9b3b3de6c5eb1732fdf
sio2 <villainalexandre89@gmail.com>
2025-11-03 13:28:07 +0100

    creation header dans le base.html.twig

--- src/Entity/Cours.php
+++ src/Entity/Cours.php
@@ -35,10 +35,10 @@ class Cours
     private ?\DateTime $heureFin = null;
 
     #[ORM\ManyToOne(inversedBy: 'cours')]
-    private ?type $type = null;
+    private ?Type $type = null;
 
     #[ORM\ManyToOne(inversedBy: 'cours')]
-    private ?jour $jour = null;
+    private ?Jour $jour = null;
 
     /**
      * @var Collection<int, inscription>
@@ -136,24 +136,24 @@ class Cours
         return $this;
     }
 
-    public function getType(): ?type
+    public function getType(): ?Type
     {
         return $this->type;
     }
 
-    public function setType(?type $type): static
+    public function setType(?Type $type): static
     {
         $this->type = $type;
 
         return $this;
     }
 
-    public function getJour(): ?jour
+    public function getJour(): ?Jour
     {
         return $this->jour;
     }
 
-    public function setJour(?jour $jour): static
+    public function setJour(?Jour $jour): static
     {
         $this->jour = $jour;
 
